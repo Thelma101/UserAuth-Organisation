a186517126f217c1a6742b9d8582efb033108db6
DCTP-BE <akpata.thelma@gmail.com>
2024-07-05 22:59:42 +0100

    v1

--- models/user.js
+++ models/user.js
@@ -1,40 +1,79 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../models/index').sequelize;
 
-
-const User = sequelize.define('User', {
-  userId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  phone: {
-    type: DataTypes.STRING,
-  },
-});
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+  });
 
-// module.exports = User;
-
-module.exports = (sequelize, DataTypes) => {
   return User;
 };
+
+
+
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../models/index').sequelize;
+
+
+// const User = sequelize.define('User', {
+//   userId: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true,
+//   },
+//   firstName: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   lastName: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true,
+//     validate: {
+//       isEmail: true,
+//     },
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   phone: {
+//     type: DataTypes.STRING,
+//   },
+// });
+
+// // module.exports = User;
+
+// module.exports = (sequelize, DataTypes) => {
+//   return User;
+// };
