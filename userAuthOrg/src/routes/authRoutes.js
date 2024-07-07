6ab4d65c260587a1daf59913ea674a210c53e3e1
DCTP-BE <akpata.thelma@gmail.com>
2024-07-07 07:55:55 +0100

    v1

--- userAuthOrg/src/routes/authRoutes.js
+++ userAuthOrg/src/routes/authRoutes.js
@@ -12,49 +12,98 @@ const secret = process.env.JWT_SECRET || 'your_jwt_secret';
 router.post('/register', validateUserFields, async (req, res) => {
     const { firstName, lastName, email, password, phone } = req.body;
     try {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const user = await prisma.user.create({
-            data: {
-                firstName,
-                lastName,
-                email,
-                password: hashedPassword,
-                phone,
-                organisations: {
-                    create: {
-                        organisation: {
-                            create: {
-                                name: `${firstName}'s Organisation`,
-                            }
-                        }
-                    }
-                }
-            }
-        });
-        const accessToken = jwt.sign({ userId: user.userId }, secret);
-        res.status(201).json({
-            status: 'success',
-            message: 'Registration successful',
-            data: {
-                accessToken,
-                user: {
-                    userId: user.userId,
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    email: user.email,
-                    phone: user.phone,
-                }
-            }
-        });
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const organisation = await prisma.organisation.create({
+        data: {
+          name: `${firstName}'s Organisation`,
+        },
+      });
+      const user = await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+          phone,
+        },
+      });
+      await prisma.userOrganisation.create({
+        data: {
+          userId: user.userId,
+          orgId: organisation.orgId,
+        },
+      });
+      const accessToken = jwt.sign({ userId: user.userId }, secret);
+      res.status(201).json({
+        status: 'uccess',
+        message: 'Registration successful',
+        data: {
+          accessToken,
+          user: {
+            userId: user.userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            phone: user.phone,
+          },
+        },
+      });
     } catch (error) {
-        res.status(400).json({
-            status: 'Bad request',
-            message: 'Registration unsuccessful',
-            statusCode: 400
-        });
-        console.error('Error during registration:', error);
+      res.status(400).json({
+        status: 'Bad request',
+        message: 'Registration unsuccessful',
+        statusCode: 400,
+      });
+      console.error('Error during registration:', error);
     }
-});
+  });
+
+// router.post('/register', validateUserFields, async (req, res) => {
+//     const { firstName, lastName, email, password, phone } = req.body;
+//     try {
+//         const hashedPassword = await bcrypt.hash(password, saltRounds);
+//         const user = await prisma.user.create({
+//             data: {
+//                 firstName,
+//                 lastName,
+//                 email,
+//                 password: hashedPassword,
+//                 phone,
+//                 organisations: {
+//                     create: {
+//                         organisation: {
+//                             create: {
+//                                 name: `${firstName}'s Organisation`,
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         });
+//         const accessToken = jwt.sign({ userId: user.userId }, secret);
+//         res.status(201).json({
+//             status: 'success',
+//             message: 'Registration successful',
+//             data: {
+//                 accessToken,
+//                 user: {
+//                     userId: user.userId,
+//                     firstName: user.firstName,
+//                     lastName: user.lastName,
+//                     email: user.email,
+//                     phone: user.phone,
+//                 }
+//             }
+//         });
+//     } catch (error) {
+//         res.status(400).json({
+//             status: 'Bad request',
+//             message: 'Registration unsuccessful',
+//             statusCode: 400
+//         });
+//         console.error('Error during registration:', error);
+//     }
+// });
 
 router.post('/login', validateLoginFields, async (req, res) => {
     const { email, password } = req.body;
