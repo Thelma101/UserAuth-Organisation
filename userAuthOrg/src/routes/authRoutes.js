5736aea1baade219404c03f662361fd940f65a73
DCTP-BE <akpata.thelma@gmail.com>
2024-07-07 02:13:14 +0100

    prisma update

--- userAuthOrg/src/routes/authRoutes.js
+++ userAuthOrg/src/routes/authRoutes.js
@@ -1,11 +1,3 @@
-// Define routes (authRoutes.js and userRoutes.js) for handling different endpoints:
-// /auth/register: Register a user and create a default organization.
-// /auth/login: Authenticate a user and return a JWT token.
-// /api/users/:id: Get user details (protected endpoint).
-// /api/organisations: Get all organizations for the logged-in user (protected endpoint).
-// /api/organisations/:orgId: Get details of a specific organization (protected endpoint).
-// /api/organisations: Create a new organization (protected endpoint).
-// /api/organisations/:orgId/users: Add a user to a specific organization (protected endpoint).
 
 const express = require('express');
 const bcrypt = require('bcrypt');
